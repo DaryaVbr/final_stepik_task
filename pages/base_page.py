83053ced5c2c8d66b461f1aab1bb5e0c67617a6a
DaryaVbr <dar.xunxun@gmail.com>
2025-05-14 17:01:43 +0300

    добавление класса наследника MainPage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,10 @@
 
 
-class BasePage():
+class BasePage:
 
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
 
     def open(self):
-        return self.browser.get(self.url)
+        self.browser.get(self.url)
